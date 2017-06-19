634b68aa74dcc17e68be5a5ae6ddba29b074f58f
michael <mikhailsobolemail@gmail.com>
2017-06-19 14:30:02 +0300

    Change names and delete regex

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from posts import views as posts_views
 
 urlpatterns = [
-    url(r'^$', posts_views.post_list),
+    url(r'^$', posts_views.post_list, name='list'),
     url(r'^create/$', posts_views.post_create),
     url(r'^(?P<id>\d+)/$', posts_views.post_detail, name='detail'),
-    url(r'^(?P<id>\d+)/edit/$', posts_views.post_update),
-    url(r'^delete/$', posts_views.post_delete),
+    url(r'^(?P<id>\d+)/edit/$', posts_views.post_update, name='update'),
+    url(r'^(?P<id>\d+)/delete/$', posts_views.post_delete, name='delete'),
 ]
