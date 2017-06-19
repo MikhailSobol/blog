bc34c4899588c715273b141f1cafd9d29ef886a2
michael <mikhailsobolemail@gmail.com>
2017-06-19 10:31:46 +0300

    Change pk to id

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from posts import views as posts_views
 urlpatterns = [
     url(r'^$', posts_views.post_list),
     url(r'^create/$', posts_views.post_create),
-    url(r'^(?P<pk>\d+)/$', posts_views.post_detail),
+    url(r'^(?P<id>\d+)/$', posts_views.post_detail, name='detail'),
     url(r'^update/$', posts_views.post_update),
     url(r'^delete/$', posts_views.post_delete),
 ]
