09edc765c7f841c732dca8431ce188f5238d06ad
michael <mikhailsobolemail@gmail.com>
2017-07-11 09:56:08 +0300

    add dynamic background

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     theme = models.CharField(max_length=64, default='Test')
+    background = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return self.title
