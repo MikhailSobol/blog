73d9c64000e474e03c78f0f12413ed4d88e4c00a
michael <mikhailsobolemail@gmail.com>
2017-07-13 16:32:50 +0300

    change urls patterns to use slug

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from posts import views as posts_views
 urlpatterns = [
     url(r'^$', posts_views.post_list, name='list'),
     url(r'^create/$', posts_views.post_create),
-    url(r'^(?P<id>\d+)/$', posts_views.post_detail, name='detail'),
-    url(r'^(?P<id>\d+)/edit/$', posts_views.post_update, name='update'),
-    url(r'^(?P<id>\d+)/delete/$', posts_views.post_delete, name='delete'),
+    url(r'^(?P<slug>[\w-]+)/$', posts_views.post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', posts_views.post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', posts_views.post_delete, name='delete'),
 ]
