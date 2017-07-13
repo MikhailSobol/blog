9d6c0bc35bb5dccd99796784842e928621ab7654
michael <mikhailsobolemail@gmail.com>
2017-07-13 16:32:34 +0300

    add slug to Post

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -9,9 +11,28 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     theme = models.CharField(max_length=64, default='Test')
     background = models.FileField(null=True, blank=True)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'id': self.id})
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug:
+        slug = new_slug
+    queryset = Post.objects.filter(slug=slug).order_by('-id')
+    if queryset.exists():
+        return create_slug(instance, new_slug='-'.join([slug, queryset.first().id]))
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
