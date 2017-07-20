b5e05ea7cd55e6f2a9dc4d3680f3cd6f33dee076
michael <mikhailsobolemail@gmail.com>
2017-07-20 16:00:31 +0300

    remove .idea

---
+++ posts/static/js/homepage.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(window).scroll(function() {
+    var add_is_showing = function($elem_class, $add_to_class, $scroll_val) {
+      if ($scroll_val > $($elem_class).offset().top - 600) {
+        $($add_to_class).each(function(i) {
+          setTimeout(function() {
+            $($add_to_class).eq(i).addClass("is-showing");
+          }, 15 * (i + 1));
+        })
+      }
+    }
+
+    var scroll = $(this).scrollTop();
+    
+    if (scroll < 300) {$(".header-text-wrapper").css({
+        "transform": "translate(0px, " + scroll / 8 + "%)"
+      });
+    }
+
+    add_is_showing(".about-wrapper", ".about-section", scroll);
+    add_is_showing(".why-section", ".why-section", scroll);
+  });
+});
