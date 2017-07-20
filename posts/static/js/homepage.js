c6e0729d3c1b4af0045f09a54fff85f676b1eaae
michael <mikhailsobolemail@gmail.com>
2017-07-20 16:36:24 +0300

    make posts text move

--- posts/static/js/homepage.js
+++ posts/static/js/homepage.js
@@ -5,15 +5,23 @@ $(document).ready(function () {
         $($add_to_class).each(function(i) {
           setTimeout(function() {
             $($add_to_class).eq(i).addClass("is-showing");
-          }, 15 * (i + 1));
+          }, 75 * (i + 1));
         })
       }
     }
 
     var scroll = $(this).scrollTop();
     
-    if (scroll < 300) {$(".header-text-wrapper").css({
+    if (scroll < 300) {
+      $(".header-text-wrapper").css({
         "transform": "translate(0px, " + scroll / 8 + "%)"
+      });
+    }
+
+    if (scroll > 1500 && scroll < 1800) {
+      console.log('lak;jsdf');
+      $(".posts-text-wrapper").css({
+        "transform": "translate(0px, " + (scroll - 1500) / 8 + "%)"
       });
     }
 
