b7224415a126c8b2510a1d2af90a836c495c310c
michael <mikhailsobolemail@gmail.com>
2017-07-21 13:28:00 +0300

    refactor animation and create make_active_with_scroll

--- posts/static/js/homepage.js
+++ posts/static/js/homepage.js
@@ -7,7 +7,7 @@ function add_is_showing ($elem_class, $add_to_class, $scroll_val) {
   if ($scroll_val > $($elem_class).offset().top - 600) {
     $($add_to_class).each(function(i) {
       setTimeout(function() {
-        $($add_to_class).eq(i).addClass("is-showing");
+        $($add_to_class).eq(i).addClass('is-showing');
       }, 75 * (i + 1));
     })
   }
@@ -15,16 +15,33 @@ function add_is_showing ($elem_class, $add_to_class, $scroll_val) {
 
 function move_text_with_scroll($scroll) {
   if ($scroll < 300) {
-    $(".header-text-wrapper").css({
-      "transform": "translate(0px, " + $scroll / 8 + "%)"
+    $('.header-text-wrapper').css({
+      'transform': 'translate(0px, ' + $scroll / 8 + '%)'
     });
   }
-    
-  if ($scroll > 1100 && $scroll < 1800) {
-    $(".posts-text-wrapper").css({
-      "transform": "translate(0px, " + ($scroll - 1500) / 8 + "%)"
-    });
+}
+
+function make_class_active_with_scroll($scroll, $class, $to_be_active) {
+  if ($scroll >= $($class).offset().top - 250) {
+    console.log('asdf');
+    make_active($to_be_active);
   }
+}
+
+function make_active_with_scroll($scroll) {
+  make_class_active_with_scroll($scroll, '.header-wrapper', '.home-scroll');
+  make_class_active_with_scroll($scroll, '.about-wrapper', '.about-scroll');
+  make_class_active_with_scroll($scroll, '.why-wrapper', '.why-scroll');
+  make_class_active_with_scroll($scroll, '.posts-wrapper', '.posts-scroll');
+}
+
+function scroll_to_section_on_click($section, $button, $coef) {
+  $($button).click(function() {
+    $('html, body').animate({
+      scrollTop: $($section).offset().top + $coef
+    }, 2000);
+  });    
+
 }
 
 $(document).ready(function () {
@@ -32,40 +49,14 @@ $(document).ready(function () {
     var scroll = $(this).scrollTop();
     
     move_text_with_scroll(scroll);
+    make_active_with_scroll(scroll);
 
-    add_is_showing(".about-wrapper", ".about-section", scroll);
-    add_is_showing(".why-section", ".why-section", scroll);
+    add_is_showing('.about-wrapper', '.about-section', scroll);
+    add_is_showing('.why-section', '.why-section', scroll);
   });
 
-  $(".home-scroll").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".header-wrapper").offset().top
-    }, 2000);
-
-    make_active('.home-scroll');
-  });    
-
-  $(".about-scroll").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".about-wrapper").offset().top - 100
-    }, 2000);
-
-    make_active('.about-scroll');
-  });    
-
-  $(".why-scroll").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".why-wrapper").offset().top - 100
-    }, 1500);
-
-    make_active('.why-scroll');
-  });    
-
-  $(".posts-scroll").click(function() {
-    $('html, body').animate({
-      scrollTop: $(".posts-wrapper").offset().top - 100
-    }, 1500);
-
-    make_active('.posts-scroll');
-  });    
+  scroll_to_section_on_click('.header-wrapper', '.home-scroll', 0);
+  scroll_to_section_on_click('.about-wrapper', '.about-scroll', -100);
+  scroll_to_section_on_click('.why-wrapper', '.why-scroll', -100);
+  scroll_to_section_on_click('.posts-wrapper', '.posts-scroll', -100);
 });
