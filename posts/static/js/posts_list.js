396115ae04516f832c2ef230463f0b8feedf2be3
michael <mikhailsobolemail@gmail.com>
2017-07-22 14:41:44 +0300

    add scripts for posts list

--- posts/static/js/posts_list.js
+++ posts/static/js/posts_list.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(this).scrollTop();
+    add_is_showing('.posts-wrapper > .container > .row', '.post',
+     scroll);
+  });
+
   $.each( jQuery('.carousel .item'), function( i, val ) {
     $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
-    // $(this).css('background-attachment', 'fixed');
   })
+
 });
