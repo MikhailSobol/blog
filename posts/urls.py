ea573a5e9caf30f5bf7c59e30686528ddfbaf045
michael <mikhailsobolemail@gmail.com>
2017-07-23 11:40:54 +0300

    add as_view()

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from posts import views as posts_views
 
 urlpatterns = [
-    url(r'^$', posts_views.post_list, name='list'),
-    url(r'^create/$', posts_views.post_create, name='create'),
-    url(r'^(?P<slug>[\w-]+)/$', posts_views.post_detail, name='detail'),
-    url(r'^(?P<slug>[\w-]+)/edit/$', posts_views.post_update, name='update'),
-    url(r'^(?P<slug>[\w-]+)/delete/$', posts_views.post_delete, name='delete'),
+    url(r'^$', posts_views.PostListView.as_view(), name='list'),
+    # url(r'^create/$', posts_views.post_create, name='create'),
+    # url(r'^(?P<slug>[\w-]+)/$', posts_views.post_detail, name='detail'),
+    # url(r'^(?P<slug>[\w-]+)/edit/$', posts_views.post_update, name='update'),
+    # url(r'^(?P<slug>[\w-]+)/delete/$', posts_views.post_delete, name='delete'),
 ]
