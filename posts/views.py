8632bc2290e1ff878f3e439db9d6413b7406a686
michael <mikhailsobolemail@gmail.com>
2017-07-23 15:44:44 +0300

    uncomment delete and detail view

--- posts/views.py
+++ posts/views.py
@@ -24,38 +24,18 @@ class HomePageView(View):
     # }
     # return render(request, "post_form.html", context)
 
+class PostDetailView(View):
+    def get(self, request, slug=None):
+        instance = get_object_or_404(Post, slug=slug)
+        context = {
+        'title': instance.title,
+        'post': instance,
+        'request': request,
+        }
+        return render(request, "post_detail.html", context)
 
-# def post_detail(request, slug=None):
-    # instance = get_object_or_404(Post, slug=slug)
-    # context = {
-        # 'title': instance.title,
-        # 'post': instance,
-        # 'request': request,
-    # }
-    # return render(request, "post_detail.html", context)
 
 
-# def post_list(request):
-    # queryset_list = Post.objects.all()
-    # most_read = queryset_list[:3]
-    # paginator = Paginator(queryset_list, 9)
-
-    # page = request.GET.get('page')
-
-    # try:
-    #     queryset = paginator.page(page)
-    # except PageNotAnInteger:
-    #     queryset = paginator.page(1)
-    # except EmptyPage:
-    #     queryset = paginator.page(paginator.num_pages)
-
-    # context = {
-    #     'post_queryset': queryset,
-    #     'most_read': most_read,
-    #     'title': 'List',
-    #     'request': request,
-    # }
-    # return render(request, "posts_list.html", context)
 class PostListView(View):
     def get(self, request):
         queryset_list = Post.objects.all()
@@ -80,28 +60,12 @@ class PostListView(View):
         return render(request, "posts_list.html", context)
 
 
-# def post_update(request, slug=None):
-    # instance = get_object_or_404(Post, slug=slug)
-    # form = PostForm(request.POST or None, instance=instance)
-
-    # if form.is_valid():
-        # return _save_instance_and_redirect(form, request, 'Saved')
-    # if form.errors:
-    #     messages.error(request, 'Not Successfully Updated')
-
-    # context = {
-    #     'title': instance.title,
-    #     'post': instance,
-    #     'form': form,
-    # }
-    # return render(request, "post_form.html", context)
-
-
-# def post_delete(request, slug=None):
-    # instance = get_object_or_404(Post, slug=slug)
-    # instance.delete()
-    # messages.success(request, 'Successfully Deleted')
-    # return redirect('list')
+class PostDeleteView(View):
+    def get(self, request, slug=None):
+        instance = get_object_or_404(Post, slug=slug)
+        instance.delete()
+        messages.success(request, 'Successfully Deleted')
+        return redirect('list')
 
 
 def _save_instance_and_redirect(form, request, text):
