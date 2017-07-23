4f0f472ee8c239015d6b181ebf40ad59d44871ea
michael <mikhailsobolemail@gmail.com>
2017-07-23 11:16:41 +0300

    add author to post

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=128)
     content = models.TextField()
     description = models.TextField()
