d42bf4ae660ec55dd7494bba4b7323721868fa86
michael <mikhailsobolemail@gmail.com>
2017-07-25 11:40:56 +0300

    change urls

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from posts import views as posts_views
 
 urlpatterns = [
     url(r'^$', posts_views.PostListView.as_view(), name='list'),
-    # url(r'^create/$', posts_views.post_create, name='create'),
+    url(r'^create/$', posts_views.PostCreateView.as_view(), name='create'),
     url(r'^(?P<slug>[\w-]+)/$', posts_views.PostDetailView.as_view(), name='detail'),
     # url(r'^(?P<slug>[\w-]+)/edit/$', posts_views.post_update, name='update'),
     url(r'^(?P<slug>[\w-]+)/delete/$', posts_views.PostDeleteView.as_view(), name='delete'),
